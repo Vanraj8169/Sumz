f8d24c7aade5f54fa3ebe8affc746028e80ac11e
Vanraj Pardeshi <vanrajpardeshi8169@gmail.com>
2023-08-09 18:08:45 +0530

    Fixed Bug and optimized code ✅

--- ArticleSummarizer/src/Components/Demo.jsx
+++ ArticleSummarizer/src/Components/Demo.jsx
@@ -1,5 +1,5 @@
-import { linkIcon } from "../assets";
-import { useState } from "react";
+import { linkIcon, loader, copy, tick } from "../assets";
+import { useEffect, useState } from "react";
 import { useLazyGetSummaryQuery } from "../services/article";
 const Demo = () => {
   const [article, setArticle] = useState({
@@ -7,14 +7,34 @@ const Demo = () => {
     summary: "",
   });
 
+  const [allArticles, setAllArticles] = useState([]);
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+  const [copied, setCopied] = useState("");
+  useEffect(() => {
+    const articleFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+    if (articleFromLocalStorage) {
+      setAllArticles(articleFromLocalStorage);
+    }
+  }, []);
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { data } = await getSummary({ articleUrl: article.url });
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
+      const updateAllArticles = [newArticle, ...allArticles];
       setArticle(newArticle);
+      setAllArticles(updateAllArticles);
+
+      localStorage.setItem("articles", JSON.stringify(updateAllArticles));
     }
+  };
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
   };
   return (
     <section className="mt-16 w-full max-w-xl">
@@ -48,9 +68,56 @@ const Demo = () => {
         </form>
 
         {/* Browser URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn" onClick={()=>handleCopy(item.url)}>
+                <img
+                  className="w-[40%] h-[40%] object-contain"
+                  src={copied === item.url ? tick : copy}
+                  alt="copy_icon"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* Display Results */}
+      <div className="my-10 max-w-full flex-justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, that wasn't supposed to happen...
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+        ;
+      </div>
     </section>
   );
 };
